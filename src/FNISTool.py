9a67827bd74236e6c51654900398a7684f01aeae
AnyOldName3 <krizdjali+github@gmail.com>
2018-09-27 22:57:39 +0100

    Double check that the user is sure when setting a non-empty mod as the output mod.

--- src/FNISTool.py
+++ src/FNISTool.py
@@ -142,7 +142,7 @@ class FNISTool(mobase.IPluginTool):
         modDirectory = self.__getModDirectory()
         isAMod = pathlibPath.parent.samefile(modDirectory)
         if not pathlibPath.is_dir() or not isAMod:
-            QMessageBox.information(self.__parentWidget, self.__tr("Choose an output mod"), self.__tr("Please choose an output mod for Fore's New Idles in Skyrim. This must be a directory in Mod Organizer's mods directory, and you can create one if you do not have one already. FNIS will delete any existing contents of this directory when it is run, so do not choose a mod you use for anything else. This setting can be updated in the Plugins tab of the Mod Organizer Settings menu."))
+            QMessageBox.information(self.__parentWidget, self.__tr("Choose an output mod"), self.__tr("Please choose an output mod for Fore's New Idles in Skyrim. This must be a directory in Mod Organizer's mods directory, and you can create one if you do not have one already. This mod will not be available to the VFS when FNIS is run, so do not choose a mod you use for anything else. This setting can be updated in the Plugins tab of the Mod Organizer Settings menu."))
             while not pathlibPath.is_dir() or not isAMod:
                 path = QFileDialog.getExistingDirectory(self.__parentWidget, self.__tr("Choose an output mod"), str(modDirectory), QFileDialog.ShowDirsOnly)
                 pathlibPath = pathlib.Path(path)
@@ -152,6 +152,19 @@ class FNISTool(mobase.IPluginTool):
                 isAMod = pathlibPath.parent.samefile(modDirectory)
                 if not isAMod:
                     QMessageBox.information(self.__parentWidget, self.__tr("Not a mod..."), self.__tr("The selected directory is not a Mod Organizer managed mod. Please choose a directory within the mods directory."))
+                    continue
+                empty = True
+                for item in pathlibPath.iterdir():
+                    if item.name != "meta.ini":
+                        empty = False
+                        break
+                if not empty:
+                    if QMessageBox.question(self.__parentWidget, self.__tr("Mod not empty"), self.__tr("The selected mod already contains files. Are you sure want to use it as the output mod?"), QMessageBox.StandardButtons(QMessageBox.Yes | QMessageBox.No)) == QMessageBox.Yes:
+                        # Proceed normally - the user is happy
+                        pass
+                    else:
+                        # Restart outer loop - the user wants to pick again
+                        isAMod = False
             # The user may have created a new mod in the MO mods directory, so we must trigger a refresh
             self.__organizer.refreshModList()
             self.__organizer.setPluginSetting(self.name(), "output-path", path)
