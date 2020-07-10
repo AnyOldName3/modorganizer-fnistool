d2af95163f2dab59868d4a6b8fc5e6ef068b8b3a
isanae <14251494+isanae@users.noreply.github.com>
2020-07-10 14:17:55 -0400

    updated translations

--- src/FNISTool_en.ts
+++ src/FNISTool_en.ts
@@ -18,32 +18,32 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISPatches.py" line="100"/>
+        <location filename="FNISPatches.py" line="101"/>
         <source>Configures the patches which FNIS applies to the game.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISPatches.py" line="97"/>
+        <location filename="FNISPatches.py" line="98"/>
         <source>FNIS/Configure FNIS Patches</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISPatches.py" line="127"/>
+        <location filename="FNISPatches.py" line="128"/>
         <source>Select Patches</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISPatches.py" line="129"/>
+        <location filename="FNISPatches.py" line="130"/>
         <source>Note: Some patches may be automatically enabled or disabled by Fore&apos;s New Idles in Skyrim, so don&apos;t be surprised if its list differs from this one.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISPatches.py" line="180"/>
+        <location filename="FNISPatches.py" line="181"/>
         <source>Unable to find FNIS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISPatches.py" line="180"/>
+        <location filename="FNISPatches.py" line="181"/>
         <source>Fore&apos;s New Idles in Skyrim can&apos;t be found using the location saved in Mod Organizer&apos;s settings. Please run the main FNIS integration tool before using this one.</source>
         <translation type="unfinished"></translation>
     </message>
@@ -66,178 +66,183 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="87"/>
+        <location filename="FNISTool.py" line="89"/>
         <source>Runs GenerateFNISforUsers.exe so the game can load custom animations.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="75"/>
+        <location filename="FNISTool.py" line="77"/>
         <source>Path to GenerateFNISforUsers.exe</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="76"/>
+        <location filename="FNISTool.py" line="78"/>
         <source>Whether or not to direct the FNIS output to a mod folder.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="77"/>
+        <location filename="FNISTool.py" line="79"/>
         <source>When output-to-mod is enabled, the path to the mod to use.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="84"/>
+        <location filename="FNISTool.py" line="86"/>
         <source>FNIS/Run FNIS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="136"/>
+        <location filename="FNISTool.py" line="138"/>
         <source>Output preference not set</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="143"/>
+        <location filename="FNISTool.py" line="145"/>
         <source>Output mod not set</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="149"/>
+        <location filename="FNISTool.py" line="151"/>
         <source>FNIS path not specified</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="149"/>
+        <location filename="FNISTool.py" line="151"/>
         <source>The path to GenerateFNISforUsers.exe wasn&apos;t specified. The tool will now exit.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="185"/>
+        <location filename="FNISTool.py" line="187"/>
         <source>Output to a mod?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="185"/>
+        <location filename="FNISTool.py" line="187"/>
         <source>Fore&apos;s New Idles in Skyrim can output either to Mod Organizer&apos;s VFS (potentially overwriting files from multiple mods) or to a separate mod. Would you like FNIS to output to a separate mod? This setting can be updated in the Plugins tab of the Mod Organizer Settings menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="255"/>
+        <location filename="FNISTool.py" line="257"/>
         <source>Choose an output mod</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="219"/>
+        <location filename="FNISTool.py" line="221"/>
         <source>Please choose an output mod for Fore&apos;s New Idles in Skyrim. This must be a directory in Mod Organizer&apos;s mods directory, and you can create one if you do not have one already. This mod will not be available to the VFS when FNIS is run, so do not choose a mod you use for anything else. This setting can be updated in the Plugins tab of the Mod Organizer Settings menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="264"/>
+        <location filename="FNISTool.py" line="266"/>
         <source>Not a mod...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="264"/>
+        <location filename="FNISTool.py" line="266"/>
         <source>The selected directory is not a Mod Organizer managed mod. Please choose a directory within the mods directory.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="276"/>
+        <location filename="FNISTool.py" line="278"/>
         <source>Mod not empty</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="276"/>
+        <location filename="FNISTool.py" line="278"/>
         <source>The selected mod already contains files. Are you sure want to use it as the output mod?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="297"/>
+        <location filename="FNISTool.py" line="299"/>
         <source>Find FNIS</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="297"/>
+        <location filename="FNISTool.py" line="299"/>
         <source>Fore&apos;s New Idles in Skyrim can&apos;t be found using the location saved in Mod Organizer&apos;s settings. Please find GenerateFNISforUsers.exe in the file picker. FNIS must be visible within the VFS, so choose an installation either within the game&apos;s data directory or within a mod folder. This setting can be updated in the Plugins tab of the Mod Organizer Settings menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="299"/>
+        <location filename="FNISTool.py" line="301"/>
         <source>Locate GenerateFNISforUsers.exe</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="311"/>
+        <location filename="FNISTool.py" line="313"/>
         <source>Not a compatible location...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="311"/>
+        <location filename="FNISTool.py" line="313"/>
         <source>Fore&apos;s New Idles in Skyrim only works when within the VFS, so must be installed to the game&apos;s data directory or within a mod folder. Please select a different FNIS installation.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="321"/>
+        <location filename="FNISTool.py" line="323"/>
         <source>FNIS mod deactivated</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="321"/>
+        <location filename="FNISTool.py" line="323"/>
         <source>Fore&apos;s New Idles in Skyrim is installed to an inactive mod. Press OK to activate it or Cancel to quit the tool</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="78"/>
+        <location filename="FNISTool.py" line="80"/>
         <source>Settings have been initialised.  Set to False to reinitialise them.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="79"/>
+        <location filename="FNISTool.py" line="81"/>
         <source>Whether or not to direct any new FNIS logs to a mod folder.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="80"/>
+        <location filename="FNISTool.py" line="82"/>
         <source>When output-logs-to-mod is enabled, the path to the mod to use.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="201"/>
+        <location filename="FNISTool.py" line="203"/>
         <source>Output logs to a mod?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="201"/>
+        <location filename="FNISTool.py" line="203"/>
         <source>Any new logs generated when running FNIS will end up in Mod Organizer&apos;s overwrite folder. Would you like these logs to be output to a separate mod? This setting can be updated in the Plugins tab of the Mod Organizer Settings menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="255"/>
+        <location filename="FNISTool.py" line="257"/>
         <source>Please choose an output mod for logs for Fore&apos;s New Idles in Skyrim. This must be a directory in Mod Organizer&apos;s mods directory, must not be the same as the FNIS output mod, and you can create one if you do not have one already. This setting can be updated in the Plugins tab of the Mod Organizer Settings menu.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="257"/>
+        <location filename="FNISTool.py" line="259"/>
         <source>Choose a log output mod</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="268"/>
+        <location filename="FNISTool.py" line="270"/>
         <source>Same as FNIS output</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="268"/>
+        <location filename="FNISTool.py" line="270"/>
         <source>The selected mod is the same as the FNIS output mod.  Please choose a different mod.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="136"/>
+        <location filename="FNISTool.py" line="138"/>
         <source>Whether or not to output to a mod was not specified. The tool will now exit.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISTool.py" line="143"/>
+        <location filename="FNISTool.py" line="145"/>
         <source>The mod to output to was not specified. The tool will now exit.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="FNISTool.py" line="76"/>
+        <source>Enable all FNISTool plugins</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -249,17 +254,17 @@
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISToolReset.py" line="35"/>
+        <location filename="FNISToolReset.py" line="36"/>
         <source>FNIS/Reset FNIS Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISToolReset.py" line="56"/>
+        <location filename="FNISToolReset.py" line="57"/>
         <source>Reset settings?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="FNISToolReset.py" line="56"/>
+        <location filename="FNISToolReset.py" line="57"/>
         <source>Would you like to reset the options that pop up when you first ran &quot;{}&quot;?</source>
         <translation type="unfinished"></translation>
     </message>
